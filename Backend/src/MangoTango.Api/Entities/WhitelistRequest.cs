b3a5e884adf6914b47a448348056df8db000fee6
Ryan de Jonge <ryandejonge@outlook.com>
2022-10-25 22:40:23 +0200

    oop

--- Backend/src/MangoTango.Api/Entities/WhitelistRequest.cs
+++ Backend/src/MangoTango.Api/Entities/WhitelistRequest.cs
@@ -2,7 +2,6 @@
 using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using XboxWebApi.Authentication;
 using static System.Net.WebRequestMethods;
 
 namespace MangoTango.Api.Entities
